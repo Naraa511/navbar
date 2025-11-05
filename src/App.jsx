1878beaf5026afa325de44040a72e08eb785c77b
Naranceceg <6822068@muls.edu.mn>
2025-11-05 12:09:32 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Example from './Navbar'
+import Header from './components/Header'
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+
+    </>
+  )
+}
+
+export default App
